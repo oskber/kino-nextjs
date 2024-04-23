d679d0ce9677f1e74043407df8c1a291aafa842d
Oskar Bergman <ocke_bergman@hotmail.com>
2024-04-23 15:54:47 +0200

    created navbar

--- kino-nextjs/app/page.tsx
+++ kino-nextjs/app/page.tsx
@@ -1,5 +1,20 @@
 import Image from 'next/image';
 
 export default function Home() {
-  return <h1>Hello homepage</h1>;
+  return (
+    <>
+      <div className="container mx-auto px-4 mt-10">
+        <h1>Hello world</h1>
+        <div className="w-full h-screen bg-green-300"></div>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+          nam vero sed voluptate animi praesentium possimus accusantium eum,
+          quisquam dicta? Molestiae earum debitis nihil necessitatibus quidem
+          eum cum eveniet ratione, vero explicabo illo, error totam quasi
+          beatae, deserunt ab quia architecto. Praesentium nobis deleniti
+          provident aliquid error. Laudantium, iste quam.
+        </p>
+      </div>
+    </>
+  );
 }
