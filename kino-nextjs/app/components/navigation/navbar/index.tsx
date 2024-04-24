d24225c7110411f26ea9aee81d223cf5eec441d5
Oskar Bergman <ocke_bergman@hotmail.com>
2024-04-24 14:15:31 +0200

    removed padding

--- kino-nextjs/app/components/navigation/navbar/index.tsx
+++ kino-nextjs/app/components/navigation/navbar/index.tsx
@@ -51,7 +51,7 @@ const NavBar = () => {
             </svg>
           </button>
           <div
-            className="hidden w-full md:block md:w-auto mr-auto ml-auto pr-12"
+            className="hidden w-full md:block md:w-auto mr-auto ml-auto"
             id="navbar-solid-bg">
             <ul className="flex flex-col font-medium mt-4 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent">
               {links.map((link) => (
