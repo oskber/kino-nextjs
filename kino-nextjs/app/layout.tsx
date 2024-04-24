4ee153ace0550c44eb688a45fa59f78348d017ac
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-04-24 22:45:25 +0200

    moved footer to navigation folder.

--- kino-nextjs/app/layout.tsx
+++ kino-nextjs/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navigation from './components/navigation/navbar/Header';
-import Footer from './components/footer/footer';
+import Footer from './components/navigation/footer/footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
