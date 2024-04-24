e4f051e955ee9fad97f8c53ebfa34d677671c6a1
Oskar Bergman <ocke_bergman@hotmail.com>
2024-04-24 14:27:43 +0200

    renamed navbar component

--- kino-nextjs/app/layout.tsx
+++ kino-nextjs/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
-import Navigation from './components/navigation/navbar';
+import Navigation from './components/navigation/navbar/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
