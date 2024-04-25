e7ac785785b2127d8bae8220851ac77a0f51ed87
ANschoolacc <145000303+ANschoolacc@users.noreply.github.com>
2024-04-25 21:16:28 +0200

    Created database movies collection and made function for fetching a single movie

--- kino-nextjs/app/lib/data.ts
+++ kino-nextjs/app/lib/data.ts
@@ -1,5 +1,8 @@
 import { moviesList } from "../../public/mockMovies";
 import { Movies } from "./types";
+import { Movie } from "./schema";
+import mongoose from "mongoose";
+import "dotenv/config";
 
 //Here we will fetch all the different data from database when it is set up
 
@@ -8,8 +11,19 @@ export default function fetchAllMovies() {
     return movies;
 }
 
-export function fetchMovie(id: string) {
-    const movies: Movies[] = moviesList;
-    const movieID: string = id;
-    return movies.find((movie) => movie.ID === movieID);
+export async function fetchMovie(id: string) {
+    try {
+        await mongoose.connect(process.env.DB_URL);
+    } catch (error) {
+        throw new Error("Failed to connect to database.");
+    }
+
+    try {
+        const movie = await Movie.findById(id);
+        await mongoose.disconnect();
+        return movie;
+    } catch (error) {
+        await mongoose.disconnect();
+        throw new Error("Failed to fetch movie with id.");
+    }
 }
