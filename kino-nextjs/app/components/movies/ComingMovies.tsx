ad858661df23bc60207466dcbd27cf20debf0d4f
MickeMarten <KarlMikael.Martensson@gmail.com>
2024-04-27 21:55:58 +0200

    Added Book-link

--- kino-nextjs/app/components/movies/ComingMovies.tsx
+++ kino-nextjs/app/components/movies/ComingMovies.tsx
@@ -1,15 +1,16 @@
 import { fetchComingMovies } from '../../lib/data';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function ComingMovies() {
   const comingMovies = await fetchComingMovies();
 
   return (
-    <section className='overflow-x-auto text-white mt-3'>
+    <section className=' text-white mt-3'>
       <div className='flex justify-center text-3xl'>
         <h2 className=''> Kommande filmer</h2>
       </div>
-      <div className='flex flex-nowrap space-x-4 py-4 px-2 md:px-0 justify-center'>
+      <div className='overflow-x-auto flex flex-nowrap space-x-4 py-4 px-2 md:px-0 justify-center'>
         {comingMovies.map((movie) => (
           <div key={movie._id} className='flex-shrink-0 w-64 max-w-xs'>
             <img
@@ -20,7 +21,12 @@ export default async function ComingMovies() {
             <div className='mt-4 flex flex-col'>
               <h1 className='text-xl font-bold'>{movie.Title}</h1>
               <span className=''>{movie.Genre}</span>
-              <button className='border'>Button component here</button>
+              <Link
+                href={`http://localhost:3000/${movie._id}`}
+                className='bg-custom_yellow w-20 text-center rounded-sm'
+              >
+                Boka
+              </Link>
             </div>
           </div>
         ))}
