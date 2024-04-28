c738b61bf145f94fb04798467f2afce897ce969a
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-04-28 18:42:23 +0200

    Add user authentication functionality

--- kino-nextjs/app/lib/schema.js
+++ kino-nextjs/app/lib/schema.js
@@ -13,4 +13,39 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.models.Movie || mongoose.model("Movie", movieSchema);
 
-export { Movie };
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Fyll i ditt förnamn'],
+    },
+    lastname: {
+        type: String,
+        required: [true, 'Fyll i ditt efternamn']
+    },
+    email: {
+        type: String,
+        required: [true, 'Fyll i en e-postadress'],
+        unique: [true, 'Ett konto med denna e-postadress finns redan registrerat'],
+        validate: {
+            validator: (value) => {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return emailRegex.test(value);
+            },
+            message: 'Fyll i en giltig e-postadress'
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Lösenordet måste vara minst 6 tecken långt'],
+        validate: {
+            validator: (value) => {
+                return value.length >= 6;
+            },
+            message: 'Lösenordet måste vara minst 6 tecken långt'
+        }
+    }
+});
+
+const userModel = mongoose.models?.User || mongoose.model('User', userSchema);
+
+export { Movie, userModel };
---
+++ kino-nextjs/auth.ts
@@ -0,0 +1,39 @@
+import NextAuth from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { authConfig } from './auth.config';
+import type { User } from './app/lib/definitions';
+import bcrypt from 'bcrypt';
+import { userModel } from './app/lib/schema';
+import 'dotenv/config';
+
+async function getUserByEmail(email: string): Promise<User | undefined> {
+    try {
+        const user = await userModel.findOne({ email });
+        return user;
+    } catch (error) {
+        throw new Error('Failed to fetch user.');
+    }
+}
+
+export const { auth, signIn, signOut } = NextAuth({
+    ...authConfig,
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                const { email, password } = credentials;
+                if (typeof email !== 'string' || typeof password !== 'string') {
+                    throw new Error('Invalid input');
+                }
+                const user = await getUserByEmail(email);
+                if (!user) {
+                    throw new Error('No user found');
+                }
+                const isValid = await bcrypt.compare(password, user.password);
+                if (!isValid) {
+                    throw new Error('Invalid password')
+                }
+                return { email: user.email, name: user.name };
+            },
+        }),
+    ],
+});
