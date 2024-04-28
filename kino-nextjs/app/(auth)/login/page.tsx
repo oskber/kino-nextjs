933d3885836e666ea5c60e0822fb7ddad6b46fd7
Max Lindström <max.lindstrom2@lerniastudent.se>
2024-04-28 19:05:47 +0200

    Update login and register page

--- kino-nextjs/app/(auth)/login/page.tsx
+++ kino-nextjs/app/(auth)/login/page.tsx
@@ -1,16 +1,13 @@
-'use client'
-
 import Link from 'next/link'
 import SigninForm from '../../components/SigninForm';
 
 export default function Page() {
   return (
-    <div className="flex justify-center items-center w-full h-screen m-auto">
-      <section className="flex flex-col justify-center items-center">
-        <SigninForm />
-        <p className="text-white text-xl mt-6 mb-2">Inget konto?</p>
-        <Link className="text-white font-semibold underline hover:opacity-80" href="/register">Registrera dig här</Link>
-      </section>
-    </div>
+    <section className="flex flex-col justify-center items-center w-full h-screen m-auto">
+      <h1 className="text-custom_yellow text-3xl mb-6">Logga in</h1>
+      <SigninForm />
+      <p className="text-white text-xl mt-6 mb-2">Inget konto?</p>
+      <Link className="text-white font-semibold underline hover:opacity-80" href="/register">Registrera dig här</Link>
+    </section>
   );
 }
--- kino-nextjs/app/(auth)/register/page.tsx
+++ kino-nextjs/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import SignupForm from '../../components/SignupForm';
 export default function Page() {
     return (
         <section className="flex flex-col justify-center items-center w-full h-screen m-auto">
-            <h1 className="text-white text-3xl mb-8">Skapa konto</h1>
+            <h1 className="text-custom_yellow text-3xl mb-6">Skapa konto</h1>
             <SignupForm />
             <p className="text-white text-xl mt-6 mb-2">Har du redan ett konto?</p>
             <a className="text-white font-semibold underline hover:opacity-80" href="/login">Logga in</a>
