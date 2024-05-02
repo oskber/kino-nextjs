58247f0b08ff1bf93e36228bccc621bb3e10e865
Oskar Bergman <ocke_bergman@hotmail.com>
2024-05-02 19:35:57 +0200

    list styling

--- kino-nextjs/app/components/reviews/ReviewsList.tsx
+++ kino-nextjs/app/components/reviews/ReviewsList.tsx
@@ -1,3 +1,4 @@
+import { StarIcon } from '@heroicons/react/20/solid';
 import { fetchReviews } from '../../lib/data';
 
 export default async function ReviewsList({ id }: { id: string }) {
@@ -9,13 +10,20 @@ export default async function ReviewsList({ id }: { id: string }) {
         <ul>
           {reviews.map((review) => (
             <li
-              className="text-white flex justify-between gap-1  border-b-2 p-2"
+              className="text-white flex justify-between gap-1 border-b-2 border-custom_yellow p-2"
               key={review._id}>
               <div className=" flex-col">
                 <h3 className="font-bold">{review.name}</h3>
                 <p>{review.comment}</p>
               </div>
-              <p>Betyg: {review.rating}</p>
+              <p className="pt-2">
+                {Array.from({ length: review.rating }).map((_, i) => (
+                  <StarIcon
+                    key={i}
+                    className="inline-block w-5 h-5 mr-0.5 text-custom_yellow"
+                  />
+                ))}
+              </p>
             </li>
           ))}
         </ul>
