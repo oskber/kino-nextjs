f21a2cf90ab277f488adf99dbad009ace8d6d015
Oskar Bergman <ocke_bergman@hotmail.com>
2024-05-13 11:47:13 +0200

    refactoring and formatting

--- kino-nextjs/app/(movies)/[id]/page.tsx
+++ kino-nextjs/app/(movies)/[id]/page.tsx
@@ -1,7 +1,7 @@
 import Movie from '../../components/movies/Movie';
 import ReviewForm from '../../components/reviews/ReviewForm';
 import ReviewsList from '../../components/reviews/ReviewsList';
-import Screenings from "../../components/movies/Screenings";
+import Screenings from '../../components/movies/Screenings';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -9,24 +9,25 @@ export const metadata: Metadata = {
 };
 
 export default async function Page({
-    params,
-    searchParams,
+  params,
+  searchParams,
 }: {
-    params: {
-        id: string;
-    };
-    searchParams?: {
-        date?: string;
-    };
+  params: {
+    id: string;
+  };
+  searchParams?: {
+    date?: string;
+    page?: string;
+  };
 }) {
-    return (
-        <main className='mt-20 grid justify-center text-white'>
-            <Movie id={params.id} />
-            <Screenings id={params.id} date={searchParams?.date} />
-            <div className='px-5 m-auto sm:w-5/6 md:w-1/2 lg:w-1/3'>
-            <ReviewForm />
-            <ReviewsList id={params.id} searchParams={searchParams} />
-            </div>
-        </main>
-    );
+  return (
+    <main className='mt-20 grid justify-center text-white'>
+      <Movie id={params.id} />
+      <Screenings id={params.id} date={searchParams?.date} />
+      <div className='px-5 m-auto sm:w-5/6 md:w-1/2 lg:w-1/3'>
+        <ReviewForm />
+        <ReviewsList id={params.id} page={searchParams?.page} />
+      </div>
+    </main>
+  );
 }
