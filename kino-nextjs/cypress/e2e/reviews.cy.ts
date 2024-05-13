f3c2ff2fcbfab20f5a418521bc4b151ac98176a9
Oskar Bergman <ocke_bergman@hotmail.com>
2024-05-13 12:25:46 +0200

    added text to test

--- kino-nextjs/cypress/e2e/reviews.cy.ts
+++ kino-nextjs/cypress/e2e/reviews.cy.ts
@@ -7,7 +7,7 @@ describe('clicks on a movie and adds a review', () => {
   it('clicks the button on a movie on homepage', () => {
     cy.get(':nth-child(1) > .pl-3 > [data-cy="movie-button"]').click();
   });
-  it('adds name, comment and 3-star rating and submits review', () => {
+  it('adds name, comment and 3-star rating and submits review. Checks if comment contains message', () => {
     cy.get('[data-cy="name-input"]').type('Test');
     cy.get('[data-cy="comment-input"]').type('success-1122334455');
     cy.get(':nth-child(3) > [data-cy="rating"]').click();
