04b9acf9a39a0674f53d1c41aff26ebf730fc509
Conny <pedalconny@hotmail.com>
2024-05-15 15:31:20 +0200

    added TicketSelector for ticket functionality

---
+++ kino-nextjs/app/components/movies/TicketSelector.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import { useTickets } from "app/contexts/TicketContext";
+
+const TicketSelector = () => {
+  const { tickets, incrementTicket, decrementTicket } = useTickets();
+
+  const renderTicketSelector = (
+    type: keyof typeof tickets,
+    label: string,
+    price: string
+  ) => {
+    return (
+      <div className='flex w-full justify-evenly my-2'>
+        <div className='flex-1 ml-20 py-3'>{label}</div>
+        <div className='flex items-center flex-1 justify-center py-2'>
+          <button
+            onClick={() => decrementTicket(type)}
+            className='px-4 py-2 mr-2 bg-custom_yellow rounded-lg'
+          >
+            -
+          </button>
+          <span className='mx-2 py-2'>{tickets[type]}</span>
+          <button
+            onClick={() => incrementTicket(type)}
+            className='px-4 py-2 ml-2 bg-custom_yellow rounded-lg'
+          >
+            +
+          </button>
+        </div>
+        <div className='flex-1 justify-end flex mr-20 py-3'>{price}</div>
+      </div>
+    );
+  };
+
+  return (
+    <div className='space-y-4 flex flex-col p-4 text-white'>
+      {renderTicketSelector("adult", "Ordinarie", "125 Kr")}
+      {renderTicketSelector("senior", "Senior", "100 Kr")}
+      {renderTicketSelector("child", "Barn", "75 Kr")}
+    </div>
+  );
+};
+
+export default TicketSelector;
