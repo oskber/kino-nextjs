44743b41c3ee7a13599cec1135b7ccf392aac983
Conny <pedalconny@hotmail.com>
2024-05-15 15:38:53 +0200

    added types for seatmatrix and booking

--- kino-nextjs/app/lib/types.ts
+++ kino-nextjs/app/lib/types.ts
@@ -1,33 +1,34 @@
 //Here we will put all bigger types and import the into different componenets
 
+export type SeatStatus = 0 | 1;
+export type SeatRow = SeatStatus[];
+export type SeatMatrix = SeatRow[];
+
 export type Tmovie = {
-    _id: String,
-    Title: String,
-    Released: Boolean,
-    Genre: String,
-    Rating: Number,
-    imdbRating: String,
-    Description: String,
-    Poster: String,
-    __v: Number,
+  _id: String;
+  Title: String;
+  Released: Boolean;
+  Genre: String;
+  Rating: Number;
+  imdbRating: String;
+  Description: String;
+  Poster: String;
+  __v: Number;
 };
 
 export type Movies = Tmovie[];
 
-
-type booking = {
-    _id: string,
-    Seats: (0 | 1)[][],
-    Price: string,
-    Email: string,
-    Date: string
-
-}
+export type booking = {
+  Seats: number[][];
+  Price: string;
+  Email: string;
+  Date: string;
+};
 
 export type screening = {
-    _id: string,
-    MovieId: string,
-    Date: string,
-    Seats: (0 | 1)[][],
-    Bookings: booking[] | [],
-}
+  _id: string;
+  MovieId: string;
+  Date: string;
+  Seats: SeatMatrix;
+  Bookings: booking[];
+};
